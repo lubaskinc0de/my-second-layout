24eabeb040bce8a2c4e566003c0ab7bcf33a5cae
lubaskinc0de <100635212+lubaskinc0de@users.noreply.github.com>
2022-03-14 23:28:12 +0300

    Add files via upload

---
+++ assets/js/text.js
@@ -0,0 +1,22 @@
+$(function() {
+    var title = document.title,
+        animSeq = ["|"],
+        animIndex = 0,
+        titleIndex = 0;
+
+    function doInverseSpinZeroPitch() {
+        var loadTitle = title.substring(0, titleIndex);
+        if (titleIndex > title.length) {
+            animIndex = 0;
+            titleIndex = 0
+        }
+        if (animIndex > 1) {
+            titleIndex++;
+            animIndex = 0
+        }
+        document.title = loadTitle + animSeq[0];
+        animIndex++
+    }
+    window.setInterval(doInverseSpinZeroPitch, 150);
+});
+$(function() { $(".typed").typed({ strings: ["lubaskin10???", "снова обогн?aл!!", "federal176????", "d!ark1n?", "su?", "pra?", "ro*ma#n?plushk0?"], typeSpeed: 50, loop: true }); });
